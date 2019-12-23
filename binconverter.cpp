9d73dc111eaa48b372459c73f38cfaf0850c69bc
patricedelmundo <53291348+patricedelmundo@users.noreply.github.com>
2019-12-23 11:23:15 -0800

    Add files via upload

---
+++ binconverter.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  binconverter
+//
+//  Created by Sherlock on 11/6/16.
+//  Copyright © 2016 Sherlock. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int arr[5];
+for (int i = 0; i <= 10; i++) {
+    if (i < 3) arr[i] = 'a'; else arr[i] = 'z';
+        cout << arr[i] << endl; }
